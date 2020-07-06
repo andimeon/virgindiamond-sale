a1af4dbf400cc491837e86d92e7ce9102a08b64f
andimeon <andimeon@gmail.com>
2020-07-06 15:31:50 +0900

    displayed prices in admin

--- jewelry/admin.py
+++ jewelry/admin.py
@@ -3,7 +3,7 @@ from .models import Type, Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['product_num', 'type', 'exist']
+    list_display = ['product_num', 'type', 'price', 'price_IJT', 'price_net', 'exist',]
     list_editable = ['exist']
     list_filter = ['type', 'exist']
     raw_id_fields = ['type', 'sold_by']
