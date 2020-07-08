c60928e0bba89491ecc50950117462839dc983b5
andimeon <andimeon@gmail.com>
2020-07-08 23:07:06 +0900

    change database to Postgres

--- Virgindiamond/settings.py
+++ Virgindiamond/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Virgindiamond.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'virgindiamond',
+        'USER': 'andimeon',
+        'PASSWORD': 'cPUXsQo6WF',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
