7ed0e3d9cb4411cb95baeec6f253229a8157d03f
andimeon <andimeon@gmail.com>
2020-07-08 23:06:49 +0900

    change database to Postgres

--- jewelry/migrations/0007_type_type_image.py
+++ jewelry/migrations/0007_type_type_image.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jewelry', '0006_auto_20200624_1714'),
+        ('jewelry', '0004_auto_20200624_1606'),
     ]
 
     operations = [
