23f7b5fbadf89a3a6ecce1af3cf6d4d8bdc76d7b
andimeon <andimeon@gmail.com>
2020-07-10 16:31:11 +0900

    Added custom domain

--- Virgindiamond/settings.py
+++ Virgindiamond/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['vg.sytechno.com']
+ALLOWED_HOSTS = ['128.199.207.134', 'vg.sytechno.com']
 
 # Application definition
 
